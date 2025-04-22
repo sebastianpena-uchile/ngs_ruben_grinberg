a0e8155041dd2f840ae44df6e46d78ab81679d94
sebastianpena-uchile <sebastianpena@uchile.cl>
2025-04-22 16:44:37 -0400

    init repo

---
+++ make.R
@@ -0,0 +1,31 @@
+#' ngs_ruben_grinberg: A Research Compendium
+#' 
+#' @description 
+#' A paragraph providing a full description of the project and describing each 
+#' step of the workflow.
+#' 
+#' @author Sebastian Pena-Frias \email{sebastianpena@uchile.cl}
+#' 
+#' @date 2025/04/22
+
+
+
+## Install Dependencies (listed in DESCRIPTION) ----
+
+devtools::install_deps(upgrade = "never")
+
+
+## Load Project Addins (R Functions and Packages) ----
+
+devtools::load_all(here::here())
+
+
+## Global Variables ----
+
+# You can list global variables here (or in a separate R script)
+
+
+## Run Project ----
+
+# List all R scripts in a sequential order and using the following form:
+# source(here::here("analyses", "script_X.R"))
